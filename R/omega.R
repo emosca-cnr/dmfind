b9c8d7c81849651fbd309819ba289ddb2fe5b936
emosca-cnr <ettore.mosca@itb.cnr.it>
2021-05-12 11:56:58 +0200

    ettore omega

--- R/omega.R
+++ R/omega.R
@@ -4,8 +4,7 @@
 #' @import igraph
 #' @export
 
-
-omega <- function(G, u) {
+omega <- function(G, u, norm=FALSE) {
 
   #implementation 2
   Gi <- igraph::induced.subgraph(G, match(names(u), V(G)$name)) # note that the vertex order is not the same as in u
@@ -17,7 +16,15 @@ omega <- function(G, u) {
   omega_vect <-  U %*% t(U)
   omega_vect <- omega_vect * Ai
   omega_vect[upper.tri(omega_vect)] <- 0
-  omega_vect <- 2*cumsum(rowSums(omega_vect))
+  #omega_vect <- 2*cumsum(rowSums(omega_vect))
+  omega_vect <- cumsum(rowSums(omega_vect))
+
+  if(norm){
+    Ai[upper.tri(Ai)] <- 0
+    nE <- cumsum(rowSums(Ai))
+    omega_vect <- omega_vect / nE
+    omega_vect[is.nan(omega_vect)] <- 0
+  }
 
   return(omega_vect)
 }
