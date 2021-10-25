43395c3dc9fdbdc5c4d9d2e79e1da652a06ab968
emosca-cnr <ettore.mosca@itb.cnr.it>
2021-10-25 16:03:43 +0200

    ettore, pc_wmz

--- R/plot_network.R
+++ R/plot_network.R
@@ -12,7 +12,7 @@
 #' @import igraph
 #' @import RColorBrewer
 
-plot_network <- function(graph, color_by=NULL, color_quant=TRUE, label_by="name", pal=NULL, plot_outfile='graph.jpg', community=NULL, comm_w_in=2, comm_w_b=1, lo=NULL, ...){
+plot_network <- function(graph, color_by=NULL, color_quant=TRUE, label_by="name", pal=NULL, plot_outfile="graph.jpg", community=NULL, comm_w_in=2, comm_w_b=1, lo=NULL, legend.off=FALSE, ...){
 
 
   if(!is.null(color_by)){
@@ -20,7 +20,7 @@ plot_network <- function(graph, color_by=NULL, color_quant=TRUE, label_by="name"
     if(color_quant){
       color_values <- cut(color_values, length(pal), dig.lab = 1)
     }
-    V(graph)$color <- pal[as.numeric(color_values)]
+    V(graph)$color <- pal[as.numeric(as.factor(color_values))]
   }
   if(label_by == ""){
     vertex_labels <- ""
@@ -37,20 +37,27 @@ plot_network <- function(graph, color_by=NULL, color_quant=TRUE, label_by="name"
   if(is.null(lo)){
     lo <- layout_with_fr(graph, weights = ew)
   }
-  jpeg(plot_outfile, width = 210, height = 200, units='mm', res=300)
-  if(!is.null(color_by)){
+  
+  if(!is.null(plot_outfile)){
+    jpeg(plot_outfile, width = 210, height = 200, units='mm', res=300)
+  }
+  
+  if(!is.null(color_by) & !legend.off){
     layout(matrix(c(1, 2), ncol = 2), widths = c(0.9, 0.2))
   }
   par(mar=c(1, 1, 1, 1))
 
   plot.igraph(graph, vertex.labels=vertex_labels, layout=lo, ...)
 
-  if(!is.null(color_by)){
+  if(!is.null(color_by) & !legend.off){
     plot.new()
     legend("bottomright", levels(factor(color_values, levels=sort(unique(color_values)))), col=pal, pch=16, bty="n", cex=0.6)
   }
 
-  dev.off()
+  if(!is.null(plot_outfile)){
+    dev.off()
+  }
+  
 
   return(lo)
 
