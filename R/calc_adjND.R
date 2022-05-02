a72005765c2f2c70a97336cd31035e6a5b708d33
Valentina <valentina.nale@itb.cnr.it>
2022-05-02 16:09:46 +0200

    vale, fix

--- R/calc_adjND.R
+++ R/calc_adjND.R
@@ -1,48 +1,48 @@
 #' Calculation of permutation-adjusted network smoothing index
-#' 
+#'
 #' @param X0 input matrix
 #' @param W symmetrically normalized adjacency matrix W = D^-1 A D^-1, see normalize_adj_mat function
 #' @param k number of permutations
 #' @param eps numeric value
 #' @param mc.cores number of cores
 #' @param returnPerm return permutations
-#' @param mode whether to use network smoothing index (S) or 
+#' @param mode whether to use network smoothing index (S) or
 #' network smoothing values (Xs)
 #' @param ... additional parameters of ND
 #' @description Calculation of permutation-adjusted network smoothing index
-#' @usage calc_adjND(X0, W, eps, k=99, mode=c("S", "Xs"), mc.cores=1, 
+#' @usage calc_adjND(X0, W, eps, k=99, mode=c("S", "Xs"), mc.cores=1,
 #' returnPerm=FALSE, ...)
-#' @examples 
-#' \dontrun{calc_adjND(X0, W, eps, k=99, mode=c("S", "Xs"), mc.cores=1, 
+#' @examples
+#' \dontrun{calc_adjND(X0, W, eps, k=99, mode=c("S", "Xs"), mc.cores=1,
 #' returnPerm=FALSE)}
 #' @return \code{data.frame} with X0, Xs, S, p and Sp
 #' @import BiocParallel
 #' @export
 #'
-calc_adjND <- function(X0, W, eps=rep(1, ncol(X0)), k=99, mode=c("S", "Xs"), 
+calc_adjND <- function(X0, W, eps=rep(1, ncol(X0)), k=99, mode=c("S", "Xs"),
                        mc.cores=1, returnPerm=FALSE, ...){
 
     mode <- match.arg(mode)
 
-    allX0 <- c(list(X0), lapply(1:k, function(x) matrix(as.numeric(X0), 
-                                ncol=ncol(X0), dimnames = 
+    allX0 <- c(list(X0), lapply(1:k, function(x) matrix(as.numeric(X0),
+                                ncol=ncol(X0), dimnames =
                                 list(sample(rownames(X0), nrow(X0))))))
-    allX0 <- BiocParallel::bplapply(allX0, function(x) 
+    allX0 <- BiocParallel::bplapply(allX0, function(x)
         x[match(rownames(W), rownames(x)), , drop=FALSE ])
 
     cat("network propagation\n")
     if(mc.cores==1){
         allXS <- lapply(allX0, function(x) ND(x, W, ...)$Xs)
     }else{
-        allXS <- BiocParallel::bplapply(allX0, function(x) 
-          ND(x, W, ...)$Xs, mc.cores=mc.cores)
+        allXS <- BiocParallel::bplapply(allX0, function(x)
+          ND(x, W, ...)$Xs)
     }
 
 
     if(mode=="S"){
         cat("calculation of Sp\n")
         #S
-        all_S <- lapply(1:(k+1), function(i) nsi(allX0[[i]], 
+        all_S <- lapply(1:(k+1), function(i) nsi(allX0[[i]],
                                                  allXS[[i]], eps=eps))
         Xs <- allXS[[1]]
     }else{
@@ -67,7 +67,7 @@ calc_adjND <- function(X0, W, eps=rep(1, ncol(X0)), k=99, mode=c("S", "Xs"),
         }
     }else{
         if(returnPerm){
-            out <- list(Xs=S, eps=eps, p=estP, Xsp= S * -log10(estP), 
+            out <- list(Xs=S, eps=eps, p=estP, Xsp= S * -log10(estP),
                         allX0=allX0, allXS=allXS)
         }else{
             out <- list(Xs=S, eps=eps, p=estP, Xsp= S * -log10(estP))
--- vignettes/vignettes_dmfind_pipeline.R
+++ vignettes/vignettes_dmfind_pipeline.R
@@ -1,3 +1,4 @@
+setwd("/home/bioinformatics/vnale/dmfind/vignettes/imgs_2")
 ########################################################################################################
 ###########################################  INTRODUCTION   ############################################
 ########################################################################################################
@@ -9,7 +10,7 @@ data(X0, W, g400esm)
 #######################################################################################################
 Xs <- ND(X0, W)$Xs
 g <- g400esm
-jpeg("X0_Xs.jpg", width = 180, height = 240, units="mm", res=300)
+jpeg("X0_Xs.jpg", width = 180, height = 180, units="mm", res=300)
 plot(X0, Xs, xlab="X0", ylab = "Xs")
 dev.off()
 #######################################################################################################
@@ -18,15 +19,6 @@ dev.off()
 eps <- matrix(c(0.01, 1, 10, 100), ncol=1)
 out <- dmfind::eval_eps(X0, Xs, eps, g)
 dmfind::plot_omega_eps(out)
-
-jpeg("eval_eps_X0.jpg", width = 180, height = 240, units="mm", res=300)
-par(mfrow=c(3, 2))
-for(i in 1:nrow(eps)){
-    plot(X0[, 1], out$S[[i]][, 1], xlab="X0", ylab="S", main = eps[i, ], pch=".")
-    top_n <- which(rank(-out$S[[i]][, 1]) <= 300)
-    points(X0[top_n, 1], out$S[[i]][top_n, 1], col="red", pch=16, cex=0.7)
-}
-dev.off() 
 #######################################################################################################
 ##################################     NETWORK SMOOTHED INDEX     #####################################
 #######################################################################################################
@@ -49,30 +41,52 @@ plot_NR(nr_res, sign_comp_table = sigcomp$sign_comp_table)
 #######################################################################################################
 scores <- scores[order(-scores$X0), ]
 compare_X0_ND(g, X0_ranked_by_ND = setNames(scores$X0, rownames(scores)),
-              X0_ranked_by_X0 = sort(setNames(scores$X0, rownames(scores)), 
+              X0_ranked_by_X0 = sort(setNames(scores$X0, rownames(scores)),
                                      decreasing = T), do.plot = T, file = "cmp_X0_ND.jpg", norm = "n")
 
-ae_res <- assess_enrichment(G=g, top_list = rownames(scores), 
-                            ranked_vector_X0 = sort(setNames(scores$X0, rownames(scores)), 
-                                                    decreasing = T), do.plot = T, critical = sigcomp$critical,
+ae_res <- assess_enrichment(G=g, top_list = rownames(scores),
+                            ranked_vector_X0 = sort(setNames(scores$X0, rownames(scores)),
+                                                    decreasing = T), do.plot = T, critical = 40,
                             file="top_net_enrichment.jpg")
 #######################################################################################################
 ######################################     TOP NETWORK     ############################################
 #######################################################################################################
 top_network <- extract_module(graph = g, selected_vertices =
-                                  nr_res$NR_summary$id, X0 = X0, 
+                                  nr_res$NR_summary$id[1:40], X0 = X0,
                               min_subnet_size = 2)
 #######################################################################################################
 #########################     COMMUNITIES AND TOPOLOGICAL ANALYISIS     ###############################
 #######################################################################################################
 res_comm <- dmfind::find_communities(top_network)
 res_comm$info
+# algorithm modularity n
+# 1 fastgreedy  0.4399270 6
+# 2    labprop  0.1974721 4
+# 3   walktrap  0.4445886 7
+# 4      eigen  0.4264225 6
+# 5   multilev  0.4562188 5 <-
+# 6    infomap  0.4538158 6
 res_comm$info$algorithm[which.max(res_comm$info$modularity)]
 res_c <- dmfind::assess_centrality(top_network, g)
+
+pal <- rainbow(length(unique(V(top_network)$comm_id)))
+lo <- plot_network(top_network, color_by = "comm_id", label_by = "", pal = pal, vertex.size=6,
+                   color_quant=F, community = res_comm$comm[[res_comm$info$algorithm[which.max(res_comm$info$modularity)]]],
+                   comm_w_in=2, comm_w_b = 1, plot_outfile = "comm_network.png")
+
 #######################################################################################################
 ################    Define nodes roles based on partecipation coefficient and z-score    ##############
 #######################################################################################################
 V(top_network)$comm_id <- res_comm$comm[[res_comm$info$algorithm[which.max(res_comm$info$modularity)]]]$membership[match(V(top_network)$name, res_comm$comm[[res_comm$info$algorithm[which.max(res_comm$info$modularity)]]]$names)]
 V(top_network)$label <- " "
-dmfind::pc_wmz(top_network, do_plot=TRUE)
+sym <- as.data.frame(org.Hs.egSYMBOL)
+V(top_network)$label <- sym$symbol[match(V(top_network)$name, sym$gene_id)]
 
+dmfind::pc_wmz(top_network, do_plot=TRUE, all_names = T)
+
+#######################################################################################################
+#####################################    Communities network    #######################################
+#######################################################################################################
+commNet <- comm_net(top_network)
+plot_network(commNet[[1]], color_by = "comm_id", pal=pal, comm_w_in = 0.1, comm_w_b = 10,
+             plot_outfile = "comm_net.jpg", legend.off = T, min_subnet_size = 2)
