fee84d9880d16b3e3f4b7bd9b442150267ed51fc
Valentina <valentina.nale@itb.cnr.it>
2022-05-04 15:16:36 +0200

    plot_eval_eps fixed

--- R/eval_eps.R
+++ R/eval_eps.R
@@ -1,5 +1,5 @@
 #' Evaluate the effect of epsilon on NSI
-#' 
+#'
 #' @description Evaluate the effect of epsilon on NSI
 #' @param X0 initial condition matrix
 #' @param Xs network smoothing result
@@ -8,7 +8,7 @@
 #' @param top top genes to consider
 #' @return list with sn_g and sn_summary elements
 #' @usage eval_eps(X0, Xs, eps=NULL, G, top)
-#' @examples 
+#' @examples
 #' \dontrun{eval_eps(X0, Xs, eps=NULL, G, top)}
 #' @import Bolstad2
 #' @export
@@ -30,20 +30,20 @@ eval_eps <- function(X0, Xs, eps=NULL, G, top=min(300, nrow(X0))){
         #order by current NSI
         nsiOrder <- apply(nsiEps[[i]], 2, function(x) order(-x)[1:top])
         #omega on X0
-        csX0[[i]] <- sapply(1:ncol(X0), function(j) 
-            omega(G, array(X0[, j], 
+        csX0[[i]] <- sapply(1:ncol(X0), function(j)
+            omega(G, array(X0[, j],
             dimnames=list(rownames(X0)))[nsiOrder[, j]]))
         #omega on Xs
-        csXs[[i]] <- sapply(1:ncol(Xs), function(j) 
-            omega(G, array(Xs[, j], 
+        csXs[[i]] <- sapply(1:ncol(Xs), function(j)
+            omega(G, array(Xs[, j],
             dimnames=list(rownames(Xs)))[nsiOrder[, j]]))
     }
 
-    aucX0 <- unlist(lapply(csX0, function(x) 
+    aucX0 <- unlist(lapply(csX0, function(x)
         Bolstad2::sintegral(x=1:top, fx=x)$int))
-    aucXs <- unlist(lapply(csXs, function(x) 
+    aucXs <- unlist(lapply(csXs, function(x)
         Bolstad2::sintegral(x=1:top, fx=x)$int))
-    out <- list(S=nsiEps, csX0=csX0, csXs=csXs, aucX0=aucX0, aucXs=aucXs)
+    out <- list(S=nsiEps, csX0=csX0, csXs=csXs, aucX0=aucX0, aucXs=aucXs, eps=eps)
 
     return(out)
 
--- R/plot_omega_eps.R
+++ R/plot_omega_eps.R
@@ -3,7 +3,7 @@
 #' @description Plot the results of eval_eps
 #' @param evalEpsRes Epsilon evaluation
 #' @param file filename
-#' @examples 
+#' @examples
 #' \dontrun{plot_omega_eps(evalEpsRes, file="eval_eps_X0_omega.jpg")}
 #' @usage plot_omega_eps(evalEpsRes, file="eval_eps_X0_omega.jpg")
 #' @return plot
@@ -18,26 +18,26 @@ plot_omega_eps <- function(evalEpsRes, file="eval_eps_X0_omega.jpg"){
     par(mfrow=c(1, 2))
     par(mar=c(4, 4, 3, 3))
 
-    plot(evalEpsRes$csX0[[1]][, 1], type='l', xlab="rank", ylab="omega(X0)", 
-         ylim=c(0, max(unlist(lapply(evalEpsRes$csX0, function(x) x[, 1])))), 
+    plot(evalEpsRes$csX0[[1]][, 1], type='l', xlab="rank", ylab="omega(X0)",
+         ylim=c(0, max(unlist(lapply(evalEpsRes$csX0, function(x) x[, 1])))),
          col=1, lty=2, cex.lab=0.8, cex.axis=0.8)
-    for(i in 2:nrow(evalEpsRes)){
+    for(i in 2:length(evalEpsRes$csX0)){
         lines(evalEpsRes$csX0[[i]][, 1], col=i, lty=2)
     }
-    legend("topleft", legend = paste("eps = ", format(evalEpsRes[, 1], digits = 2), 
-           "; AUC = ", format(evalEpsRes$aucX0, digits = 2), sep=""), lty=1, 
-           col=1:nrow(evalEpsRes), xpd=NA, cex=0.4)
+    legend("bottomright", legend = paste("eps = ", format(evalEpsRes$eps, digits = 2),
+           "; AUC = ", format(evalEpsRes$aucX0, digits = 2), sep=""), lty=1,
+           col=1:length(evalEpsRes$csX0), xpd=NA, cex=0.4)
 
-    plot(evalEpsRes$csXs[[1]][, 1], type='l', xlab="rank", ylab="omega(Xs)", 
-         ylim=c(0, max(unlist(lapply(evalEpsRes$csXs, function(x) x[, 1])))), 
+    plot(evalEpsRes$csXs[[1]][, 1], type='l', xlab="rank", ylab="omega(Xs)",
+         ylim=c(0, max(unlist(lapply(evalEpsRes$csXs, function(x) x[, 1])))),
          col=1, lty=2, cex.lab=0.8, cex.axis=0.8)
-    for(i in 2:nrow(evalEpsRes)){
+    for(i in 2:length(evalEpsRes$csXs)){
         lines(evalEpsRes$csXs[[i]][, 1], col=i, lty=2)
     }
 
-    legend("topleft", legend = paste("eps = ", format(evalEpsRes[, 1], digits = 2), 
-          "; AUC = ", format(evalEpsRes$aucXs, digits = 2), sep=""), lty=1, 
-          col=1:nrow(evalEpsRes), xpd=NA, cex=0.4)
+    legend("bottomright", legend = paste("eps = ", format(evalEpsRes$eps, digits = 2),
+          "; AUC = ", format(evalEpsRes$aucXs, digits = 2), sep=""), lty=1,
+          col=1:length(evalEpsRes$csXs), xpd=NA, cex=0.4)
 
     dev.off()
 
