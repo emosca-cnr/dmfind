66bf46469072d94256baf0db2f1a9ea36a7daa2e
emosca-cnr <ettore.mosca@itb.cnr.it>
2023-10-02 16:51:46 +0200

    ettore, string v12.0

--- R/PPI.R
+++ R/PPI.R
@@ -1,3 +1,3 @@
-#' Gene-gene interactions from STRING db
-#' Data frame with high confidence (>=700) gene-gene interactions. For every gene, also the top 3 interactions in the range [400, 699] are considered. Genes are identified with Entrez Gene identifiers.
+#' Gene-gene interactions from STRING db v12.0
+#' Data frame with high confidence (>=700) gene-gene interactions, without text mining. For every gene, the top 3 interactions in the range [400, 699] are also considered. Genes are represented by their Entrez Gene identifier.
 "PPI"
